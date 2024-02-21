d69e07c28965e775e86f33b348b8ad89912da241
Parthkk17 <kaneriya.parth@gmail.com>
2024-02-21 17:23:32 +0530

    user json add & photo

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ import { ProjectCard } from "./ProjectCard";
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
-      <h2 className={styles.title}>Projects</h2>
+      <h2 className={styles.title}>Core Team</h2>
       <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
